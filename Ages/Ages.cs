a362afc85021cc460c4446809006e463b6ce688e
ellirajkovska <50946592+ellirajkovska@users.noreply.github.com>
2023-03-04 03:10:34 +0200

    update new problems Basic Syntax, Conditional Statements and Loops - Exercise

---
+++ Ages/Ages.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Ages
+{
+    class Ages
+    {
+
+        //Write a program that determines if a person is baby, child, teenager, adult or elder 
+        //based on the given age.The boundaries are:
+        //•	0-2 – baby
+        //•	3-13 – child
+        //•	14-19 – teenager
+        //•	20-65 – adult
+        //•	>= 66 – elder
+        //•	All the values are inclusive.
+
+        //1-st  option invoke PrintAge(input) method-----------------------------------------------------
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            PrintAge(age);
+
+
+        }
+        public static void PrintAge(int input)
+        {
+            string message = string.Empty;
+            if (0 <= input && input < 3)
+
+            {
+                Console.WriteLine("baby");
+            }
+            else if (3 <= input && input < 14)
+
+            {
+                Console.WriteLine("child");
+            }
+            else if (14 <= input && input < 20)
+
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (20 <= input && input < 66)
+
+            {
+                Console.WriteLine("adult");
+            }
+            else if (66 <= input)
+
+            {
+                Console.WriteLine("elder");
+
+            }
+
+
+
+
+            // 2-nd option - single Main method ----------------------------------------------------------------------
+
+            /// static void Main(string[] args)
+            //        {
+            //            int age = int.Parse(Console.ReadLine());
+
+            //            if (0 <= age && age< 3)
+
+            //            {
+            //                Console.WriteLine("baby");
+            //            }
+            //            else if (3 <= age && age< 14)
+
+            //            {
+            //                Console.WriteLine("child");
+            //            }
+            //            else if (14 <= age && age < 20)
+            //            {
+            //                Console.WriteLine("teenager");
+            //            }
+            //            else if (20 <= age && age < 66)
+            //            {
+            //                Console.WriteLine("adult");
+            //            }
+            //            else if (66 <= age)
+            //            {
+            //                Console.WriteLine("elder");
+        }
+        //}
+    }
+}
---
+++ Vacation/Vacation.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Vacation
+    {
+        static void Main(string[] args)
+        {
+                                        //            You will receive three lines from the console:
+                                        //•	A count of people who are going on vacation.
+                                        //•	Type of the group(Students, Business or Regular).
+                                        //•	The day of the week which the group will stay on(Friday, Saturday or Sunday).
+                                        //Based on the given information calculate how much the group will pay for the entire vacation.
+                                        //The price for a single person is as follows:
+	                                                                    // Friday  Saturday    Sunday
+                                                              //Students    8.45    9.80    10.46
+                                                              //Business    10.90   15.60   16
+                                                              //Regular      15     20      22.50
+                                        //There are also discounts based on some conditions:
+                                        //•	For Students – if the group is 30 or more people, you should reduce the total price by 15 %.
+                                        //•	For Business – if the group is 100 or more people, 10 of the people stay for free.
+                                        //•	For Regular – if the group is between 10 and 20  people(both inclusively), reduce the total price by 5 %.
+            
+                                            //Note: You should reduce the prices in that EXACT order!
+                                        //As an output print the final price which the group is going to pay in the format: 
+                                        //"Total price: {price}"
+                                        //The price should be formatted to the second decimal point.
+
+
+
+            // only IF-s -------------------------------------------------------
+            int customersNumber = int.Parse(Console.ReadLine());
+            string customersType = Console.ReadLine();
+            string checkInDay = Console.ReadLine();
+            double price = 0;
+            double finalPrice = 0;
+            if (customersType == "Students")
+            {
+                if (checkInDay == "Friday")
+                {
+                    price = customersNumber * 8.45;
+                    finalPrice = price;
+                }
+                else if (checkInDay == "Saturday")
+                {
+                    price = customersNumber * 9.8;
+                    finalPrice = price;
+                }
+                else if (checkInDay == "Sunday")
+                {
+                    price = customersNumber * 10.46;
+                    finalPrice = price;
+                }
+                if (customersNumber >= 30)
+                {
+                    finalPrice = finalPrice * 0.85;
+                }
+            }
+            else if (customersType == "Business")
+            {
+                if (checkInDay == "Friday")
+                {
+                    price = customersNumber * 10.9;
+                    finalPrice = price;
+                }
+                else if (checkInDay == "Saturday")
+                {
+                    price = customersNumber * 15.6;
+                    finalPrice = price;
+                }
+                else if (checkInDay == "Sunday")
+                {
+                    price = customersNumber * 16;
+                        finalPrice = price;
+                    
+                }
+                if (customersNumber >= 100)
+                {
+                    finalPrice -= (price / customersNumber) * 10;
+                }
+            }
+            else if (customersType == "Regular")
+            {
+                if (checkInDay == "Friday")
+                {
+                    price = customersNumber * 15;
+                    finalPrice = price;
+                }
+                if (checkInDay == "Saturday")
+                {
+                    price = customersNumber * 20;
+                    finalPrice = price;
+
+                }
+                if (checkInDay == "Sunday")
+                {
+                    price = customersNumber * 22.5;
+                        finalPrice = price;
+                    
+                }
+                if (10 <= customersNumber && customersNumber <= 20)
+                {
+                    finalPrice = finalPrice * 0.95;
+                }
+            }
+
+            Console.WriteLine($"Total price: {finalPrice:f2}");
+
+
+            // 1st option - combining if and switch 
+            //int customersNumber = int.Parse(Console.ReadLine());
+            //string customersType = Console.ReadLine();
+            //string checkInDay = Console.ReadLine();
+            //double price = 0;
+            //double finalPrice = 0;
+            //if (customersType == "Students")
+            //{
+            //    switch (checkInDay)
+            //    {
+            //        case "Friday":
+            //            price = customersNumber * 8.45;
+            //            finalPrice = price;
+            //            break;
+            //        case "Saturday":
+            //            price = customersNumber * 9.8;
+            //            finalPrice = price;
+            //            break;
+            //        case "Sunday":
+            //            price = customersNumber * 10.46;
+            //            finalPrice = price; 
+            //            break;
+            //    }
+            //    if (customersNumber >= 30)
+            //    {
+            //        finalPrice = finalPrice * 0.85;
+            //    }
+            //}
+            //else if (customersType == "Business")
+            //{
+            //    switch (checkInDay)
+            //    {
+            //        case "Friday":
+            //            price = customersNumber * 10.9;
+            //            finalPrice = price;
+            //            break;
+            //        case "Saturday":
+            //            price = customersNumber * 15.6;
+            //            finalPrice = price;
+            //            break;
+            //        case "Sunday":
+            //            price = customersNumber * 16;
+            //            finalPrice = price;
+            //            break;
+            //    }
+            //    if (customersNumber >= 100)
+            //    {
+            //        finalPrice -= (price /customersNumber)* 10;
+            //    }
+            //}
+            //else if (customersType == "Regular")
+            //{
+            //    switch (checkInDay)
+            //    {
+            //        case "Friday":
+            //            price = customersNumber * 15;
+            //            finalPrice = price; 
+            //            break;
+            //        case "Saturday":
+            //            price = customersNumber * 20;
+            //            finalPrice = price; 
+            //            break;
+            //        case "Sunday":
+            //            price = customersNumber * 22.5;
+            //            finalPrice = price;
+            //            break;
+            //    }
+            //    if (10 <= customersNumber && customersNumber <= 20)
+            //    {
+            //        finalPrice = finalPrice * 0.95;
+            //    }
+            //}
+
+            //Console.WriteLine($"Total price: {finalPrice:f2}");
+        }
+    }
+}
